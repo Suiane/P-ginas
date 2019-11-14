95cfa4b63175c8dee91fc9c9bc54844107589259
Aurilucy Silva Gonçalves <2017118050058@ifpi.int>
2019-11-14 08:25:36 -0300

    CRUD de posts

---
+++ posts/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from.models import Post
+from django.urls import reverse_lazy
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['titulo', 'autor', 'texto']
+
+class ListagemView(ListView):
+    model = Post
+    template_name = 'listagem.html'
+    context_object_name = 'lista'
+    
+class PostagemDetailView(DetailView): # Nova classe
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delet.html'
+    success_url = reverse_lazy('listagem')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['titulo', 'texto']
